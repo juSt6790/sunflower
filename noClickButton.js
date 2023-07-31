408a2051be14cf20f7050f3153f8eb10d67b489d
juSt6790 <120416303+juSt6790@users.noreply.github.com>
2023-07-31 21:05:36 +0530

    Update noClickButton.js

--- noClickButton.js
+++ noClickButton.js
@@ -2,5 +2,5 @@ const yes = document.querySelector(".yes");
 const no = document.querySelector(".no");
 function loff() {
     const ques = document.querySelector(".question");
-    ques.innerHTML="Okay ILoffYou Anywayyyy :) <3";
+    ques.innerHTML="Okay I Love You Anywayyyy :) <3";
 }
