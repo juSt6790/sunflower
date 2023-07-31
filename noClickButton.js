07873c9757f66b392c36ea12812c2244019876da
juSt6790 <120416303+juSt6790@users.noreply.github.com>
2023-07-31 21:45:29 +0530

    Add files via upload

---
+++ noClickButton.js
@@ -0,0 +1,7 @@
+var yes = document.querySelector(".yes");
+var no = document.querySelector(".no");
+function loff() {
+    const ques = document.querySelector(".question");
+    ques.style.transition = '1s';
+    ques.innerHTML = "Okay I Love You Anywayy :) <3";
+}
