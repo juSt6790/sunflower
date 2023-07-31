e8265d9a527a9464c5598a18cb965546729b58ea
juSt6790 <120416303+juSt6790@users.noreply.github.com>
2023-07-31 21:21:18 +0530

    Delete noClickButton.js

--- noClickButton.js
+++
@@ -1,6 +0,0 @@
-const yes = document.querySelector(".yes");
-const no = document.querySelector(".no");
-function loff() {
-    const ques = document.querySelector(".question");
-    ques.innerHTML="Okay I Love You Anywayy :) <3";
-}
