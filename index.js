8f1f46f6dc832dbea74292036f9767888ebdcc90
juSt6790 <120416303+juSt6790@users.noreply.github.com>
2023-07-31 21:20:50 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const yes = document.querySelector(".yes");
-const no = document.querySelector(".no");
-function loff() {
-    const ques = document.querySelector(".question");
-    ques.innerHTML="ILoffYou Tooooo :) <3";
-}
